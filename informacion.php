fd260c1d86915e041f380552d128efe57b7e995b
dilantor17 <dilantor17@gmail.com>
2025-08-12 16:37:58 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$identificacion= $_POST [' identificacion' ];
+$NomApellidos=$_POST [' NomApellidos' ];
+$Cargo=$_POST [' Cargo' ];
+$Salario=$_POST [' Salario' ];
+echo " la identificacion del empleado es: ".Identificacion;
+echo " Los nombre y apellidos del empleado son:".NomApellidos;
+echo "El cargo del empleado es: ".Cargo;
+echo "El salario del empleado es: ".salario;
+?>
